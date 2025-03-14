dd93e886ace54aa2d2f342df2fc28ad6e75d366f
AdamHaniff <AdamHaniffDeveloper@gmail.com>
2025-03-14 18:03:44 -0400

    Added validation for the input fields

--- src/Form.js
+++ src/Form.js
@@ -11,7 +11,31 @@ function Form() {
 
   // HANDLER FUNCTIONS
   function handleChange(e) {
-    dispatch(updateField({ name: e.target.name, value: e.target.value }));
+    let { name, value } = e.target;
+    const numValue = parseFloat(value);
+
+    // Allow only numbers and decimals (no letters)
+    if (!/^\d*\.?\d*$/.test(value)) return;
+
+    // Prevent more than 2 decimal places
+    if (value.includes(".") && value.split(".")[1].length > 2) return;
+
+    // Specific validation for mortgage amount
+    if (name === "amount") {
+      if (numValue > 10_000_000) return;
+    }
+
+    // Specific validation for mortgage term
+    if (name === "term") {
+      if (numValue > 50 || numValue < 1 || value.includes(".")) return;
+    }
+
+    // Specific validation for mortgage rate
+    if (name === "rate") {
+      if (numValue > 30 || numValue < 0.1) return;
+    }
+
+    dispatch(updateField({ name, value }));
   }
 
   return (
